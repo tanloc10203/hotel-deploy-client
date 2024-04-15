5db296223ad41af1b997aebb31f6e92a64f0c360
LocPhan <ginga550504@gmail.com>
2024-04-15 17:06:33 +0700

    deploy code

---
+++ src/sections/auth/register/RegisterForm.jsx
@@ -0,0 +1,141 @@
+import { Form, FormikProvider, useFormik } from "formik";
+import { useState } from "react";
+// material
+import { LoadingButton } from "@mui/lab";
+import { IconButton, InputAdornment, Stack, TextField } from "@mui/material";
+// component
+import { useSelector } from "react-redux";
+import { authState } from "~/features/authentication/authSlice";
+import { RegisterSchema } from "~/utils";
+import Iconify from "../../../components/Iconify";
+
+// ----------------------------------------------------------------------
+
+export default function RegisterForm({ onSubmit }) {
+  const { isLoading } = useSelector(authState);
+
+  // console.log(isLoading);
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      phone: "",
+      username: "",
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: (values) => {
+      if (!onSubmit) return;
+
+      const newData = {
+        first_name: values.firstName,
+        last_name: values.lastName,
+        email: values.email,
+        password: values.password,
+        phone: values.phone,
+        username: values.username,
+      };
+
+      onSubmit(newData);
+    },
+  });
+
+  const { errors, touched, handleSubmit, isSubmitting, getFieldProps } = formik;
+
+  return (
+    <FormikProvider value={formik}>
+      <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+        <Stack spacing={3}>
+          <Stack direction={{ xs: "column", sm: "row" }} spacing={2}>
+            <TextField
+              fullWidth
+              label="Họ"
+              {...getFieldProps("firstName")}
+              error={Boolean(touched.firstName && errors.firstName)}
+              helperText={touched.firstName && errors.firstName}
+            />
+
+            <TextField
+              fullWidth
+              label="Tên"
+              {...getFieldProps("lastName")}
+              error={Boolean(touched.lastName && errors.lastName)}
+              helperText={touched.lastName && errors.lastName}
+            />
+          </Stack>
+
+          <TextField
+            fullWidth
+            autoComplete="email"
+            type="email"
+            label="Địa chỉ E-mail"
+            {...getFieldProps("email")}
+            error={Boolean(touched.email && errors.email)}
+            helperText={
+              (touched.email && errors.email) ||
+              "Nhập chính xác địa chỉ email của bạn"
+            }
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="phone"
+            type="phone"
+            label="Số điện thoại"
+            {...getFieldProps("phone")}
+            error={Boolean(touched.phone && errors.phone)}
+            helperText={touched.phone && errors.phone}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="username"
+            type="text"
+            label="Tài khoản"
+            {...getFieldProps("username")}
+            error={Boolean(touched.username && errors.username)}
+            helperText={touched.username && errors.username}
+          />
+
+          <TextField
+            fullWidth
+            autoComplete="current-password"
+            type={showPassword ? "text" : "password"}
+            label="Mật khẩu"
+            {...getFieldProps("password")}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                  >
+                    <Iconify
+                      icon={showPassword ? "eva:eye-fill" : "eva:eye-off-fill"}
+                    />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            error={Boolean(touched.password && errors.password)}
+            helperText={touched.password && errors.password}
+          />
+
+          <LoadingButton
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            loading={isLoading}
+          >
+            Đăng ký
+          </LoadingButton>
+        </Stack>
+      </Form>
+    </FormikProvider>
+  );
+}
